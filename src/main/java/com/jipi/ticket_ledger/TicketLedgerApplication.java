cd5328f520ba5a0ff9f8875b14b3895f59b6540c
jipi <198682663+jpjg-dev@users.noreply.github.com>
2026-04-14 02:57:05 +0900

    chore : init setting

---
+++ src/main/java/com/jipi/ticket_ledger/TicketLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.jipi.ticket_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jipi/ticket_ledger/TicketLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jipi.ticket_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
