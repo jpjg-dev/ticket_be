6d048c5f365d4b169e911558194db89f480d342a
jipi <198682663+jpjg-dev@users.noreply.github.com>
2026-06-08 17:21:51 +0900

    test : SimpleGranted 구현체에서 GrantedAuthority Interface 로 변경으로 인한 테스트코드 수정

--- src/test/java/com/jipi/ticket_ledger/user/presentation/UserControllerTest.java
+++ src/test/java/com/jipi/ticket_ledger/user/presentation/UserControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -134,10 +135,13 @@ class UserControllerTest {
     }
 
     private UsernamePasswordAuthenticationToken authenticationPrincipal(Long userId) {
+        List<GrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_USER")
+        );
         return new UsernamePasswordAuthenticationToken(
                 userId,
                 null,
-                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+                authorities
         );
     }
 }
